94a08ae32835b2d7bc786017d5b4dbcf609e2dc3
DanielM2292 <143299403+DanielM2292@users.noreply.github.com>
2024-09-19 22:57:26 -0500

    CRUD completo

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
 //import { Express } from 'express'
 const express = require("express");
-
 const mongoose = require("mongoose");
+const Product = require("./models/products.model");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
   res.send("Hola desde Node API");
 });
+
+app.get('/api/products', async(req, res) => {
+    try{
+        const product = await Product.find({})
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/product/:id', async(req, res) => {
+    try{
+        const {id} = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    }catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/api/product/:id', async(req, res) =>{
+    try{
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+
+        if(!product){
+            return res.status(404).json({message: "El producto no existe"})
+        }
+
+        const updateProduct = await Product.findById(id)
+        res.status(200).json(updateProduct)
+
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/api/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const productDelete = await Product.findByIdAndDelete(id)
+
+        if(!productDelete){
+            return res.status(404).json({message: 'Producto no encontrado'})
+        }
+
+        res.status(200).json({message: 'Producto eliminado'})
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose
   .connect(
