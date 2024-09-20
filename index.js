299ff4839d01dd4bb944c9623da8556bbc7a9454
DanielM2292 <143299403+DanielM2292@users.noreply.github.com>
2024-09-19 22:03:09 -0500

    creacion de products.model.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 //import { Express } from 'express'
-const express = require('express')
+const express = require("express");
 
-const mongoose = require('mongoose')
-const app = express()
+const mongoose = require("mongoose");
+const app = express();
 
-app.listen(4000, () => {
-    console.log("Sever Corriendo en el Puerto 4000")
-})
+app.get("/", (req, res) => {
+  res.send("Hola desde Node API");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hola desde Node API')
-})
-
-mongoose.connect("mongodb+srv://danielns871:lMoAjJ9UsxLUWTo7@cluster0.ptr3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => {
-        console.log("OK")
-    })
-    .catch(() => {
-        console.log("Fallo")
-    })
+mongoose
+  .connect(
+    "mongodb+srv://danielns871:lMoAjJ9UsxLUWTo7@cluster0.ptr3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("OK conexión exitosa");
+    app.listen(4000, () => {
+      console.log("Sever Corriendo en el Puerto 4000");
+    });
+  })
+  .catch(() => {
+    console.log("Fallo");
+  });
