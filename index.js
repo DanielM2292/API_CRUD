0efee866716a5380ab89e8d316d73e853527f0a0
DanielM2292 <143299403+DanielM2292@users.noreply.github.com>
2024-09-19 21:39:38 -0500

    Coneccion Con Mongo DB

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 //import { Express } from 'express'
-
 const express = require('express')
+
+const mongoose = require('mongoose')
 const app = express()
 
 app.listen(4000, () => {
-    console.log("Sever Corriendo en el Puerto 3000")
+    console.log("Sever Corriendo en el Puerto 4000")
 })
 
 app.get('/', (req, res) => {
-    res.send('Hola desde Node API en curso')
+    res.send('Hola desde Node API')
 })
+
+mongoose.connect("mongodb+srv://danielns871:lMoAjJ9UsxLUWTo7@cluster0.ptr3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log("OK")
+    })
+    .catch(() => {
+        console.log("Fallo")
+    })
